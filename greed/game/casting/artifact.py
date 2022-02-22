94c2b84ba6cbecb76864c164884ab3ab3f44a2e3
BelChaparro <a.belenchaparro@gmail.com>
2022-02-22 11:26:58 -0300

    created the artifact's method and attribute. updated comments

--- greed/game/casting/artifact.py
+++ greed/game/casting/artifact.py
@@ -3,29 +3,22 @@ from game.casting.actor import Actor
 
 class Artifact(Actor):
     """
-    An item of cultural or historical interest. 
+    A rock or a gem. 
     
-    The responsibility of an Artifact is to provide a message about itself.
+    The responsibility of an Artifact is to provide a value for the player
+    to win or lose a point. Rock(o) == -1. Gem(*) == +1
 
     Attributes:
-        _message (string): A short description about the artifact.
+        _value (int): The value of the artifact [-1 / +1].
     """
-    def __init__(self):
+    def __init__(self, value):
         super().__init__()
-        self._message = ""
+        self._value = value
         
-    def get_message(self):
-        """Gets the artifact's message.
+    def get_value(self):
+        """Gets the artifact's value [-1 / +1].
         
         Returns:
-            string: The message.
+            int: The value.
         """
-        return self._message
-    
-    def set_message(self, message):
-        """Updates the message to the given one.
-        
-        Args:
-            message (string): The given message.
-        """
-        self._message = message
+        return self._value
